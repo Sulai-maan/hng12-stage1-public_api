fbdc737ee55f2e8e8bcfc0c08731b972d60e9631
Sulai-maan <sulaimonsalako20@gmail.com>
2025-02-05 17:53:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from props import error_reponse, is_prime, is_perfect, is_armstrong, is_even_or_odd, digit_sum, fun_fact
+from props import error_reponse, is_prime, is_perfect, is_armstrong, is_even_or_odd, digit_sum, get_fun_fact
 
 app = FastAPI()
 
@@ -18,7 +18,7 @@ async def get(number):
         "is_perfect": is_perfect(number),
         "properties": (["armstrong"] if is_armstrong(number) else []) + [is_even_or_odd(number)],
         "digit_sum": digit_sum(number),
-        "fun_fact": fun_fact(number)
+        "fun_fact": get_fun_fact(number)
     }
 
     return response_dict
