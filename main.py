7d18dbfb24409f89d3b5b5f943dc987aa3963935
Sulai-maan <sulaimonsalako20@gmail.com>
2025-02-05 14:58:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-from props import error_reponse, is_prime, is_perfect, is_armstrong, is_even, digit_sum, fun_fact
+from props import error_reponse, is_prime, is_perfect, is_armstrong, is_even_or_odd, digit_sum, fun_fact
 
 app = FastAPI()
 
 @app.get("/api/classify-number/{number}")
 async def get(number):
-    #do some edge checking
     if "." in number:
         return error_reponse
     try:
@@ -17,7 +16,7 @@ async def get(number):
         "number": number,
         "is_prime": is_prime(number),
         "is_perfect": is_perfect(number),
-        "properties": (["armstrong"] if is_armstrong(number) else []) + [is_even(number)],
+        "properties": (["armstrong"] if is_armstrong(number) else []) + [is_even_or_odd(number)],
         "digit_sum": digit_sum(number),
         "fun_fact": fun_fact(number)
     }
